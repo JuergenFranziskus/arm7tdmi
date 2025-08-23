3488c64fde1980327c0f1fd1c9c0800a0d73546a
Juergen Franziskus <felix.hppt@gmail.com>
2025-08-23 21:03:00 +0200

    Pull SingleStepTests as submodule

--- src/decode.rs
+++ src/decode.rs
@@ -1,4 +1,4 @@
-use crate::instruction::Instruction;
+use crate::instruction::{Condition, DataOperation, Instruction, Operand2, Operation, Register, Shift, ShiftCount, ShiftType, WordTransferOffset};
 
 
 #[derive(Copy, Clone, Debug)]
@@ -9,7 +9,273 @@ impl ArmDecoder {
     }
 
     pub fn decode(self) -> Instruction {
-        todo!()
+        let condition = self.decode_condition();
+
+        let operation = match self.major() {
+            0b000 | 0b001 => self.decode_major_zero_one(),
+            0b011 if self.decode_flag(7) => Operation::Undefined,
+            0b010 | 0b011 => self.decode_single_data_transfer(),
+            0b100 => self.decode_block_data_transfer(),
+            0b101 => self.decode_branch(),
+            0b110 => self.decode_coprocessor_data_transfer(),
+            8.. => unreachable!(),
+            or => todo!("cannot decode major opcode {or:0>3b}"),
+        };
+
+        Instruction {
+            condition,
+            operation,
+        }
+    }
+    fn decode_condition(&self) -> Condition {
+        let nibble = self.0 >> 28;
+        match nibble {
+            0 => Condition::Equal,
+            1 => Condition::NotEqual,
+            2 => Condition::UnsignedHigherOrSame,
+            3 => Condition::UnsignedLower,
+            4 => Condition::Negative,
+            5 => Condition::PositiveOrZero,
+            6 => Condition::Overflow,
+            7 => Condition::NoOverflow,
+            8 => Condition::UnsignedHigher,
+            9 => Condition::UnsignedLowerOrSame,
+            10 => Condition::GreaterOrEqual,
+            11 => Condition::LessThan,
+            12 => Condition::GreaterThan,
+            13 => Condition::LessThanOrEqual,
+            14 => Condition::Always,
+            15 => Condition::Reserved,
+            _ => unreachable!(),
+        }
+    }
+
+
+
+    fn decode_major_zero_one(&self) -> Operation {
+        if ((self.0 & 0x0FFFFFF0) >> 4) == 0b0001_0010_1111_1111_1111_0001 {
+            self.decode_branch_and_exchange()
+        }
+        else if self.is_mrs() {
+            self.decode_mrs()
+        }
+        else if self.is_msr() {
+            self.decode_msr()
+        }
+        else if self.is_msr_flg() {
+            self.decode_msr_flg()
+        }
+        else {
+            self.decode_data_operation()
+        }
+    }
+    fn is_mrs(&self) -> bool {
+        (self.0 & 0xFFF) == 0
+            && (self.0 >> 16) & 0x3F == 0b001111
+            && (self.0 >> 23) & 0x1F == 0b00010
+    }
+    fn is_msr(&self) -> bool {
+        (self.0 >> 4) & 0x3FFFF == 0b101001111100000000
+            && (self.0 >> 23) & 0x1F == 0b00010
+    }
+    fn is_msr_flg(&self) -> bool {
+        (self.0 >> 4) & 0x3FFFF == 0b1010001111
+            && ((self.0 >> 23) & 0x1F == 0b00010
+                || (self.0 >> 23) & 0x1F == 0b00110)
+    }
+
+    fn decode_branch_and_exchange(&self) -> Operation {
+        let rn = self.decode_register(0);
+        Operation::Bx(rn)
+    }
+    fn decode_mrs(&self) -> Operation {
+        let rd = self.decode_register(12);
+        let p = self.decode_flag(22);
+        if p {
+            Operation::MoveFromSpsr(rd)
+        }
+        else {
+            Operation::MoveFromCpsr(rd)
+        }
+    }
+    fn decode_msr(&self) -> Operation {
+        let rm = self.decode_register(0);
+        let p = self.decode_flag(22);
+        if p {
+            Operation::MoveToSpsr(rm)
+        }
+        else {
+            Operation::MoveToCpsr(rm)
+        }
+    }
+    fn decode_msr_flg(&self) -> Operation {
+        let src = self.decode_operand_2(self.decode_flag(25));
+        let p = self.decode_flag(22);
+        if p {
+            Operation::MoveToSpsrf(src)
+        }
+        else {
+            Operation::MoveToCpsrf(src)
+        }
+    }
+    fn decode_data_operation(&self) -> Operation {
+        let i = self.decode_flag(25);
+        let s = self.decode_flag(20);
+        let opcode = (self.0 >> 21) as u8 & 0xF;
+        let operation = match opcode {
+            0 => DataOperation::And,
+            1 => DataOperation::Eor,
+            2 => DataOperation::Sub,
+            3 => DataOperation::Rsb,
+            4 => DataOperation::Add,
+            5 => DataOperation::Adc,
+            6 => DataOperation::Sbc,
+            7 => DataOperation::Rsc,
+            8 => DataOperation::Tst,
+            9 => DataOperation::Teq,
+            10 => DataOperation::Cmp,
+            11 => DataOperation::Cmn,
+            12 => DataOperation::Orr,
+            13 => DataOperation::Mov,
+            14 => DataOperation::Bic,
+            15 => DataOperation::Mvn,
+            16.. => unreachable!(),
+        };
+
+        let rn = self.decode_register(16);
+        let rd = self.decode_register(12);
+        let operand2 = self.decode_operand_2(i);
+        Operation::Data {
+            operation,
+            s,
+            rd,
+            rn,
+            operand2,
+        }
+    }
+
+
+
+    fn decode_single_data_transfer(&self) -> Operation {
+        let i = self.decode_flag(25);
+        let p = self.decode_flag(24);
+        let u = self.decode_flag(23);
+        let b = self.decode_flag(22);
+        let w = self.decode_flag(21);
+        let l = self.decode_flag(20);
+        let rn = self.decode_register(16);
+        let rd = self.decode_register(12);
+        let offset = if i {
+            WordTransferOffset::Shift(self.decode_shift())
+        }
+        else {
+            WordTransferOffset::Immediate((self.0 & 0xFFF) as u16)
+        };
+
+        Operation::WordTransfer { p, u, b, w, l, rn, rd, offset }
+    }
+    fn decode_block_data_transfer(&self) -> Operation {
+        let p = self.decode_flag(24);
+        let u = self.decode_flag(23);
+        let s = self.decode_flag(22);
+        let w = self.decode_flag(21);
+        let l = self.decode_flag(20);
+        let rn = self.decode_register(16);
+        let register_list = self.0 as u16;
+
+        Operation::BlockTransfer { p, u, s, w, l, rn, register_list }
+    }
+    fn decode_branch(&self) -> Operation {
+        let l = self.decode_flag(24);
+        let offset = self.0 & 0xFFFFFF;
+        let offset_sign = offset & (1 << 23) != 0;
+        let offset_high = if offset_sign { 0xFF000000 } else { 0 };
+        let offset = offset | offset_high;
+        let offset = (offset as i32) << 2;
+        if l {
+            Operation::Bl(offset)
+        }
+        else {
+            Operation::B(offset)
+        }
+    }
+    fn decode_coprocessor_data_transfer(&self) -> Operation {
+        let p = self.decode_flag(24);
+        let u=  self.decode_flag(23);
+        let n = self.decode_flag(22);
+        let w = self.decode_flag(21);
+        let l = self.decode_flag(20);
+
+        let rn = self.decode_register(16);
+        let crd = self.decode_register(12);
+        let cp_number = (self.0 >> 8) as u8 & 0xF;
+        let offset = self.0 as u8;
+
+        Operation::CoprocessorMemoryTransfer {
+            p,
+            u,
+            n,
+            w,
+            l,
+            rn,
+            crd,
+            cp_number,
+            offset,
+        }
+    }
+
+    fn decode_operand_2(&self, i: bool) -> Operand2 {
+        if i {
+            let imm = self.0 & 0xFF;
+            let shift = (self.0 >> 8) & 0xF;
+            let imm = imm.rotate_right(shift * 2);
+            Operand2::Immediate(imm as i32)
+        }
+        else {
+            Operand2::Register(self.decode_shift())
+        }
+    }
+
+    fn decode_shift(&self) -> Shift {
+        let rm = self.decode_register(0);
+        let reg = self.decode_flag(4);
+        let shift_type = (self.0 >> 5) & 0x3;
+        let shift_type = match shift_type {
+            0 => ShiftType::LogicalLeft,
+            1 => ShiftType::LogicalRight,
+            2 => ShiftType::ArithmeticRight,
+            3 => ShiftType::RotateRight,
+            4.. => unreachable!(),
+        };
+
+        if reg {
+            let rs = self.decode_register(8);
+            Shift {
+                rm,
+                shift_type,
+                shift_count: ShiftCount::Register(rs),
+            }
+        }
+        else {
+            let amount = (self.0 >> 7) as u8 & 0x1F;
+            Shift {
+                rm,
+                shift_type,
+                shift_count: ShiftCount::Constant(amount),
+            }
+        }
+    }
+
+    fn decode_flag(&self, at: u32) -> bool {
+        let mask = 1 << at;
+        self.0 & mask != 0
+    }
+    fn decode_register(&self, at: u32) -> Register {
+        let code = (self.0 >> at) as u8 & 0xF;
+        Register::new(code)
+    }
+    fn major(&self) -> u8 {
+        (self.0 >> 25) as u8 & 0x7
     }
 }
 
--- src/instruction.rs
+++ src/instruction.rs
@@ -1,9 +1,17 @@
+use std::io::{self, Write};
+
 
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Instruction {
     pub condition: Condition,
     pub operation: Operation,
+}
+impl Instruction {
+    pub fn pretty_print(self, out: &mut impl Write, address: Option<u32>) -> io::Result<()> {
+        self.operation.pretty_print(out, self.condition, address)?;
+        Ok(())
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -23,6 +31,32 @@ pub enum Condition {
     GreaterThan,
     LessThanOrEqual,
     Always,
+    Reserved,
+}
+impl Condition {
+    pub fn pretty_print(self, out: &mut impl Write) -> io::Result<()> {
+        use Condition::*;
+        match self {
+            Equal => write!(out, "EQ")?,
+            NotEqual => write!(out, "NE")?,
+            UnsignedHigherOrSame => write!(out, "CS")?,
+            UnsignedLower => write!(out, "CC")?,
+            Negative => write!(out, "MI")?,
+            PositiveOrZero => write!(out, "PL")?,
+            Overflow => write!(out, "VS")?,
+            NoOverflow => write!(out, "VC")?,
+            UnsignedHigher => write!(out, "HI")?,
+            UnsignedLowerOrSame => write!(out, "LS")?,
+            GreaterOrEqual => write!(out, "GE")?,
+            LessThan => write!(out, "LT")?,
+            GreaterThan => write!(out, "GT")?,
+            LessThanOrEqual => write!(out, "LE")?,
+            Always => (),
+            Reserved => write!(out, "RESERVED_CONDITION!!")?,
+        }
+
+        Ok(())
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -39,8 +73,10 @@ pub enum Operation {
     },
     MoveFromCpsr(Register),
     MoveFromSpsr(Register),
-    MoveToCpsr(Field, Register, Operand2),
-    MoveToSpsr(Field, Register, Operand2),
+    MoveToCpsr(Register),
+    MoveToSpsr(Register),
+    MoveToCpsrf(Operand2),
+    MoveToSpsrf(Operand2),
     Mul {
         s: bool,
         rd: Register,
@@ -130,7 +166,179 @@ pub enum Operation {
     },
     Undefined,
 }
+impl Operation {
+    pub fn pretty_print(self, out: &mut impl Write, condition: Condition, address: Option<u32>) -> io::Result<()> {
+        match self {
+            Operation::Bx(rn) => {
+                write!(out, "BX")?;
+                condition.pretty_print(out)?;
+                write!(out, " ")?;
+                rn.pretty_print(out)?;
+            }
+            Operation::B(offset) => {
+                write!(out, "B")?;
+                condition.pretty_print(out)?;
+                let sign = if offset < 0 { "-" } else { "" };
+                let abs = offset.abs();
+                write!(out, " {sign}{abs:x}")?;
 
+                if let Some(address) = address {
+                    let target = address.wrapping_add_signed(offset).wrapping_add(8);
+                    write!(out, " => {target:0>8x}")?;
+                }
+            }
+            Operation::Bl(offset) => {
+                write!(out, "BL")?;
+                condition.pretty_print(out)?;
+                let sign = if offset < 0 { "-" } else { "" };
+                let abs = offset.abs();
+                write!(out, " {sign}{abs:x}")?;
+
+                if let Some(address) = address {
+                    let target = address.wrapping_add_signed(offset).wrapping_add(8);
+                    write!(out, " => {target:0>8x}")?;
+                }
+            }
+            Operation::Data { operation, s, rd, rn, operand2 } => {
+                operation.pretty_print(out)?;
+                condition.pretty_print(out)?;
+                if s { write!(out, "S")?; }
+                write!(out, " ")?;
+                if operation.has_result() {
+                    rd.pretty_print(out)?;
+                    write!(out, ", ")?;
+                }
+                if !operation.is_unary() {
+                    rn.pretty_print(out)?;
+                    write!(out, ", ")?;
+                }
+                operand2.pretty_print(out)?;
+            }
+            Operation::MoveFromCpsr(rd) => {
+                write!(out, "MRS")?;
+                condition.pretty_print(out)?;
+                write!(out, " ")?;
+                rd.pretty_print(out)?;
+                write!(out, ", CPSR")?;
+            }
+            Operation::MoveFromSpsr(rd) => {
+                write!(out, "MRS")?;
+                condition.pretty_print(out)?;
+                write!(out, " ")?;
+                rd.pretty_print(out)?;
+                write!(out, ", SPSR")?;
+            }
+            Operation::MoveToCpsr(rm) => {
+                write!(out, "MSR")?;
+                condition.pretty_print(out)?;
+                write!(out, " CPSR, ")?;
+                rm.pretty_print(out)?;
+            }
+            Operation::MoveToSpsr(rm) => {
+                write!(out, "MSR")?;
+                condition.pretty_print(out)?;
+                write!(out, " SPSR, ")?;
+                rm.pretty_print(out)?;
+            }
+            Operation::MoveToCpsrf(rm) => {
+                write!(out, "MSR")?;
+                condition.pretty_print(out)?;
+                write!(out, " CPSR_flg, ")?;
+                rm.pretty_print(out)?;
+            }
+            Operation::MoveToSpsrf(rm) => {
+                write!(out, "MSR")?;
+                condition.pretty_print(out)?;
+                write!(out, " SPSR_flg, ")?;
+                rm.pretty_print(out)?;
+            }
+            Operation::WordTransfer { p, u, b, w, l, rn, rd, offset } => {
+                let name = if l { "LDR" } else { "STR" };
+                let byte_suffix = if b { "B" } else { "" };
+                let force_user_mode = if !p && w { "T" } else { "" };
+                let sign = if u { "+" } else {"-" };
+                write!(out, "{name}")?;
+                condition.pretty_print(out)?;
+                write!(out, "{byte_suffix}{force_user_mode} ")?;
+                rd.pretty_print(out)?;
+                write!(out, ", [")?;
+                rn.pretty_print(out)?;
+
+                if !p {
+                    write!(out, "], ")?;
+                }
+                else {
+                    write!(out, " ")?;
+                }
+                write!(out, "{sign} ")?;
+                offset.pretty_print(out)?;
+
+                if p {
+                    write!(out, "]")?;
+                    if w {
+                        write!(out, "!")?;
+                    }
+                }
+            }
+            Operation::BlockTransfer { p, u, s, w, l, rn, mut register_list } => {
+                let name = if l { "LDM" } else { "STM" } ;
+                let suffix = match (p, u) {
+                    (true, true) => "IB",
+                    (false, true) => "IA",
+                    (true, false) => "DB",
+                    (false, false) => "DA",
+                };
+                let write_back = if w { "!" } else { "" };
+                let caret = if s { "^" } else { "" };
+
+                write!(out, "{name}{suffix} ")?;
+                rn.pretty_print(out)?;
+                write!(out, "{write_back}, {{")?;
+                for i in 0..16 {
+                    let mask = 1 << i;
+                    if register_list & mask == 0 { continue };
+                    Register::new(i).pretty_print(out)?;
+                    register_list &= !mask;
+                    if register_list != 0 {
+                        write!(out, ", ")?;
+                    }
+                }
+                write!(out, "}}{caret}")?;
+
+            }
+            Operation::Undefined => write!(out, "UNDEFINED")?,
+            Operation::CoprocessorMemoryTransfer { p, u, n, w, l, rn, crd, cp_number, offset } => {
+                let name = if l { "LDC" } else { "STC" };
+                let long = if n { "L" } else { "" };
+                write!(out, "{name}")?;
+                condition.pretty_print(out)?;
+                write!(out, "{long} {cp_number}#, c")?;
+                crd.pretty_print(out)?;
+                write!(out, ", [")?;
+
+                rn.pretty_print(out)?;
+
+                if !p {
+                    write!(out, "], ")?;
+                }
+                else {
+                    write!(out, " ")?;
+                }
+                let sign = if u { "+" } else {"-" };
+                write!(out, "{sign} {offset:x}")?;
+
+                if p {
+                    write!(out, "]")?;
+                    if w {
+                        write!(out, "!")?;
+                    }
+                }
+            }
+            _ => write!(out, "UNPRINTABLE")?,
+        }
+        Ok(())
+    }
+}
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum DataOperation {
@@ -151,48 +359,89 @@ pub enum DataOperation {
     Bic,
     Mvn,
 }
+impl DataOperation {
+    pub fn pretty_print(self, out: &mut impl Write) -> io::Result<()> {
+        use DataOperation::*;
+        let print = match self {
+            And => "AND",
+            Eor => "EOR",
+            Sub => "SUB",
+            Rsb => "RSB",
+            Add => "ADD",
+            Adc => "ADC",
+            Sbc => "SBC",
+            Rsc => "RSC",
+            Tst => "TST",
+            Teq => "TEQ",
+            Cmp => "CMP",
+            Cmn => "CMN",
+            Orr => "ORR",
+            Mov => "MOV",
+            Bic => "BIC",
+            Mvn => "MVN",
+        };
+        write!(out, "{print}")?;
+        Ok(())
+    }
 
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub struct Field(u8);
-impl Field {
-    const F: u8 = 1 << 0;
-    const S: u8 = 1 << 1;
-    const X: u8 = 1 << 2;
-    const C: u8 = 1 << 3;
-
-    pub fn f(self) -> bool { self.0 & Self::F != 0 }
-    pub fn s(self) -> bool { self.0 & Self::S != 0 }
-    pub fn x(self) -> bool { self.0 & Self::X != 0 }
-    pub fn c(self) -> bool { self.0 & Self::C != 0 }
-
-    pub fn set_f(&mut self, to: bool) { self.0 &= !Self::F; if to { self.0 |= Self::F; }}
-    pub fn set_s(&mut self, to: bool) { self.0 &= !Self::S; if to { self.0 |= Self::S; }}
-    pub fn set_x(&mut self, to: bool) { self.0 &= !Self::X; if to { self.0 |= Self::X; }}
-    pub fn set_c(&mut self, to: bool) { self.0 &= !Self::C; if to { self.0 |= Self::C; }}
-
-    pub fn with_f(mut self, to: bool) -> Self { self.set_f(to); self }
-    pub fn with_s(mut self, to: bool) -> Self { self.set_s(to); self }
-    pub fn with_x(mut self, to: bool) -> Self { self.set_x(to); self }
-    pub fn with_c(mut self, to: bool) -> Self { self.set_c(to); self }
+    pub fn has_result(self) -> bool {
+        use DataOperation::*;
+        !matches!(self, Cmp | Cmn | Teq | Tst)
+    }
+    pub fn is_unary(self) -> bool {
+        use DataOperation::*;
+        matches!(self, Mov | Mvn)
+    }
 }
+
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Register(u8);
+impl Register {
+    pub fn new(code: u8) -> Self {
+        assert!(code < 0x10);
+        Self(code)
+    }
 
+    pub fn pretty_print(self, out: &mut impl Write) -> io::Result<()> {
+        write!(out, "r{}", self.0)?;
+        Ok(())
+    }
+}
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Operand2 {
     Register(Shift),
-    Immediate(u8, u8),
+    Immediate(i32),
+}
+impl Operand2 {
+    pub fn pretty_print(self, out: &mut impl Write) -> io::Result<()> {
+        match self {
+            Self::Register(shift) => shift.pretty_print(out)?,
+            Self::Immediate(value) => {
+                let sign = if value < 0 { "-" } else { "" };
+                let abs = value.abs();
+                write!(out, "#{sign}{abs:x}")?;
+            }
+        }
+
+        Ok(())
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Shift {
     pub rm: Register,
     pub shift_type: ShiftType,
-    pub shift_count: Operand2ShiftCount,
+    pub shift_count: ShiftCount,
 
+}
+impl Shift {
+    pub fn pretty_print(self, out: &mut impl Write) -> io::Result<()> {
+        write!(out, "UNPRINTABLE_SHIFT")?;
+
+        Ok(())
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -203,7 +452,7 @@ pub enum ShiftType {
     RotateRight,
 }
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Operand2ShiftCount {
+pub enum ShiftCount {
     Constant(u8),
     Register(Register),
 }
@@ -212,6 +461,16 @@ pub enum Operand2ShiftCount {
 pub enum WordTransferOffset {
     Immediate(u16),
     Shift(Shift),
+}
+impl WordTransferOffset {
+    pub fn pretty_print(self, out: &mut impl Write) -> io::Result<()> {
+        match self {
+            Self::Immediate(offset) => write!(out, "#{offset:0>4x}")?,
+            Self::Shift(shift) => shift.pretty_print(out)?,
+        }
+
+        Ok(())
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
